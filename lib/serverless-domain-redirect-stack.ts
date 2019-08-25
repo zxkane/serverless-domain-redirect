0dd0a0a0105a001112f92e2f92986096bf4c8768
Kane Zhu <kane.mx@gmail.com>
2019-08-25 23:53:28 +0800

    Create s3 bucket for redirecting.

--- lib/serverless-domain-redirect-stack.ts
+++ lib/serverless-domain-redirect-stack.ts
@@ -1,9 +1,18 @@
 import cdk = require('@aws-cdk/core');
+import s3 = require("@aws-cdk/aws-s3");
 
 export class ServerlessDomainRedirectStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // create S3 bucket to redirect the request
+    const targetHost = this.node.tryGetContext('targetHost');
+    if (targetHost) {
+      const bucket = new s3.Bucket(this, 'ServerlessDomainRedirectBucket',{
+        websiteRedirect: { hostName: targetHost }
+      });
+    } else {
+      throw new Error(`Please specify 'targetHost' via context.`);
+    }
   }
 }
